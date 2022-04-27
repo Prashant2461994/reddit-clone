19789572f8a5edf9380d51789fd6c37751bdc142
Prashant <sumiit.prashant.june@gmail.com>
2022-04-27 20:38:50 +0530

    get PostService interface

--- redit-backend/src/main/java/com/redditbackend/app/mapper/PostMapper.java
+++ redit-backend/src/main/java/com/redditbackend/app/mapper/PostMapper.java
@@ -16,7 +16,7 @@ public interface PostMapper {
 	@Mapping(target = "subreddit", source = "subreddit")
 	@Mapping(target = "description", source = "postRequest.description")
 	@Mapping(target = "user", source = "user")
-	public abstract Post map(PostRequest postRequest, Subreddit subreddit, User user);
+	public abstract Post mapPost(PostRequest postRequest, Subreddit subreddit, User user);
 
 	@Mapping(target = "id", source = "postId")
 	@Mapping(target = "postName", source = "postName")
@@ -24,5 +24,5 @@ public interface PostMapper {
 	@Mapping(target = "description", source = "description")
 	@Mapping(target = "userName", source = "user.username")
 	@Mapping(target = "subredditName", source = "subreddit.name")
-	public abstract PostResponse mapDto(Post post);
+	public abstract PostResponse mapPostDto(Post post);
 }
--- redit-backend/src/main/java/com/redditbackend/app/service/PostService.java
+++ redit-backend/src/main/java/com/redditbackend/app/service/PostService.java
@@ -1,15 +1,21 @@
 package com.redditbackend.app.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.redditbackend.app.dto.PostRequest;
+import com.redditbackend.app.dto.PostResponse;
+import com.redditbackend.app.exceptions.PostNotFoundException;
 import com.redditbackend.app.exceptions.SubredditNotFoundException;
 import com.redditbackend.app.mapper.PostMapper;
 import com.redditbackend.app.model.Post;
 import com.redditbackend.app.model.Subreddit;
-import com.redditbackend.app.model.User;
+import com.redditbackend.app.repository.PostRepository;
 import com.redditbackend.app.repository.SubredditRepository;
+import static java.util.List.*;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,10 +29,22 @@ public class PostService {
 	private final SubredditRepository subredditRepository;
 	private final AuthService authService;
 	private final PostMapper postMapper;
+	private final PostRepository postRepository;
 
 	public Post save(PostRequest postRequest) {
-		Subreddit subreddit = subredditRepository.findByName(postRequest.getPostName()).orElseThrow(() -> new SubredditNotFoundException(postRequest.getSubredditName()));
-		User currentUser = authService.getCurrentUser();
-		return postMapper.map(postRequest, subreddit, currentUser);
+		Subreddit subreddit = subredditRepository.findByName(postRequest.getPostName())
+				.orElseThrow(() -> new SubredditNotFoundException(postRequest.getSubredditName()));
+		return postRepository.save(postMapper.mapPost(postRequest, subreddit, authService.getCurrentUser()));
+	}
+
+	@Transactional(readOnly = true)
+	public PostResponse getPost(Long id) {
+		Post post = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException(id.toString()));
+		return postMapper.mapPostDto(post);
+	}
+
+	@Transactional(readOnly = true)
+	public List<PostResponse> getAllPosts() {
+		return postRepository.findAll().stream().map(postMapper::mapPostDto).collect(Collectors.toList());
 	}
 }
