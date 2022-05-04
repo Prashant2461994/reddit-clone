cda682ed42cb7690c9bba79dd77af8635845368e
Prashant <sumiit.prashant.june@gmail.com>
2022-05-04 16:11:59 +0530

    fixing angular and spring multi module project

--- redit-backend/target/generated-sources/annotations/com/redditbackend/app/mapper/PostMapperImpl.java
+++ redit-backend/target/generated-sources/annotations/com/redditbackend/app/mapper/PostMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-04T16:02:47+0530",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.15 (Private Build)"
+    date = "2022-05-04T16:10:00+0530",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 1.4.50.v20210914-1429, environment: Java 17.0.2 (Eclipse Adoptium)"
 )
 @Component
 public class PostMapperImpl extends PostMapper {
