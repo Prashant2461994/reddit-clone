9961192de2235c6e9ddbf00955ed326bd57bdc12
Prashant <sumiit.prashant.june@gmail.com>
2022-05-08 00:09:13 +0530

    set postgres driver scope to provided

--- redit-backend/target/generated-sources/annotations/com/redditbackend/app/mapper/PostMapperImpl.java
+++ redit-backend/target/generated-sources/annotations/com/redditbackend/app/mapper/PostMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-07T23:51:44+0530",
+    date = "2022-05-08T00:08:52+0530",
     comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 1.4.50.v20210914-1429, environment: Java 17.0.2 (Eclipse Adoptium)"
 )
 @Component
